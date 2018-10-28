7ccc3fc51c324562ab2e479a5791553a87062788
LeeWongSnail <wangli_0632@163.com>
2018-10-28 12:16:52 +0800

    init

---
+++ LWOSSURLSplice/LWOSSURLSplice/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LWOSSURLSplice
+//
+//  Created by LeeWong on 2018/10/28.
+//  Copyright © 2018 LeeWong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LWOSSURLSplice/LWOSSURLSplice/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  LWOSSURLSplice
+//
+//  Created by LeeWong on 2018/10/28.
+//  Copyright © 2018 LeeWong. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
